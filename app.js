71a0bc2117db569df86c31f7d0f533a276286fee
alexiscenriquez <alexiscenriquez@hotmail.com>
2022-01-03 01:47:57 -0600

    copy function

--- app.js
+++ app.js
@@ -1,37 +1,40 @@
-let values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-let divs = document.querySelectorAll(".color");
-let code = document.querySelectorAll(".code");
+const values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const divs = document.querySelectorAll(".color");
+const code = document.querySelectorAll(".code");
+const button = document.querySelector("button");
+const body = document.querySelector("body");
+const wrapper = document.querySelectorAll(".wrapper");
+const icon = document.querySelectorAll("i");
 let hex;
 let random;
-let newWord;
-let button = document.querySelector("button");
-let body = document.querySelector("body");
-let icon = document.querySelectorAll("i");
+const arr = [];
 
 window.onload = generate;
+button.onclick = generate;
 window.addEventListener("keypress", (e) => {
   if (e.code === "Space") {
     generate();
   }
 });
-button.onclick = generate;
+
 function generate() {
   for (let i = 0; i < divs.length; i++) {
     hex = "#";
     for (let i = 0; i < 6; i++) {
       random = Math.floor(Math.random() * values.length - 1) + 1;
       hex = hex.concat(values[random]);
+     
     }
     divs[i].style.backgroundColor = hex;
     code[i].textContent = hex;
+    arr.push(hex)
   }
 }
-
-for (let i = 0; i < divs.length; i++) {
-  divs[i].addEventListener("mouseover", () => {
+for (let i = 0; i < wrapper.length; i++) {
+  wrapper[i].addEventListener("mouseover", () => {
     icon[i].classList.add("show");
   });
-  divs[i].addEventListener("mouseleave", () => {
+  wrapper[i].addEventListener("mouseout", () => {
     icon[i].classList.remove("show");
   });
 }
@@ -43,10 +46,21 @@ for (let i = 0; i < icon.length; i++) {
 }
 window.addEventListener("keypress", (e) => {
   if (e.code === "KeyC") {
+    
+copyToClipboard(arr.toString())
   }
 });
 
-async function writeClipImg() {
-  try {
-  } catch {}
+function copyToClipboard(text) {
+var type="text/plain"
+var blob=new Blob([text],{type})
+var data=[new ClipboardItem({[type]:blob})]
+navigator.clipboard.write(data).then(
+function(){
+  console.log("success")
+},
+function(){
+  console.log("failure")
+}
+);
 }
